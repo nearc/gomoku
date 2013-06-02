42a21559caf98de6e02b578f3eb47760f0f640c3
CHEN <nearc116@hotmail.com>
2013-06-02 17:14:14 +0800

    [DEV]add some comments [DEV]

--- mainwindow.hpp
+++ mainwindow.hpp
@@ -32,6 +32,10 @@ private:
     int actX;
     int actY;
 
+    //*利用vetor+二进制的思想存放棋盘和棋子颜色的情况
+    //*比如boardState.at(0)==9==8+1
+    //*表示第一行的1, 4有落子
+    //*颜色的表达也类似
     std::vector<int> boardState;//0 no chess, 1 engaged
     std::vector<int> colorState;//1 black, 0 white
 
@@ -40,8 +44,8 @@ protected:
     void mousePressEvent(QMouseEvent *);
 
 private slots:
-    void boardCleaned();
-    void chessPlaced();
+    void boardCleaned();//用于清空棋盘
+    void chessPlaced();//用于向胜负判断函数传递有新子落下的消息
 
 signals:
     void mouseClicked();
